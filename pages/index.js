44dd6d4ad7f62daae315316ce5d58a8fdce02db8
fatihsenel <fatih_senel@msn.com>
2020-06-05 15:36:34 +0300

    ana sayfa giriş yazısı değişti

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ function HomePage() {
       <Head>
         <title>Ana sayfa</title>
       </Head>
-      <h1>Welcome to Next.js!</h1>
+      <h1>İlk web siteme hoş geldin!</h1>
       <style jsx>{``}</style>
     </Layout>
   )
